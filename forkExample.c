f680e3e9a55b6a11949a0174c71b0ed2a537c745
elg5195 <evan.gutman@gmail.com>
2016-03-17 20:55:48 -0400

    update: pipes semi-working

--- forkExample.c
+++ forkExample.c
@@ -8,6 +8,13 @@
 #include <stdlib.h>
 #include <stdio.h> 
 
+//Node structure for linked list
+struct word_t {
+	char *word;
+	int count;
+	struct word_t *next;
+};
+
 main(int argc, char *argv[])
 {
 	FILE *fp;
@@ -21,9 +28,21 @@ main(int argc, char *argv[])
 	int startpointParent;
 	int endpointParent;
 	char c;
+	int fd[2];
+
+
+
+
+	// if (pipe(fd) == -1) {
+ //        perror("pipe");
+ //        exit(1);
+ //    }
+    
+
+
 
 	startpoint=ftell(fp);
-	printf("%d\n\n", startpoint);
+	//printf("%d\n\n", startpoint);
 
 	fseek(fp, 0, SEEK_END);
 	sz = ftell(fp);
@@ -57,17 +76,56 @@ main(int argc, char *argv[])
 			endpoint = EOF;
 		else{
 			fseek(fp, endpoint, SEEK_SET);
-			while ((c=fgetc(fp))!=EOF && c != 32 && c != 10);
+			while ((c=fgetc(fp))!= EOF && c != 32 && c != 10);
 			endpoint = ftell(fp);
 			fseek(fp, startpoint, SEEK_SET);
 		}
+		position=i;
 
-		child_pid=fork ();
+		
+		if (pipe(fd) == -1) {
+        	perror("pipe failed");
+        	exit(1);
+   		}
+   		
+
+		child_pid = fork ();
 		if (child_pid == 0)
 			break;
 	}
-	if (child_pid > 0)
-		printf("parent with range: (%d,%d)\n", startpointParent, endpointParent);
-	else if (child_pid == 0)
-		printf("child with range: (%d,%d)\n", startpoint, endpoint);
+
+
+
+	if (child_pid > 0){  //parent
+		startpoint = startpointParent;
+		endpoint = endpointParent;
+
+		struct word_t *buf;
+		buf = (struct word_t *)malloc(sizeof(struct word_t));
+		for(i = 0; i < n-1; i++){
+			read(fd[0], buf, sizeof(struct word_t) * 2);
+			close(fd[0]);
+			printf("%s: %d\n%s: %d\n", buf->word, buf->count, buf->next->word, buf->next->count);
+		}
+		//wait(NULL);
+
+	}
+	else if (child_pid == 0){ //child
+		//printf("child\n");
+		struct word_t *num;
+		num = (struct word_t *)malloc(sizeof(struct word_t));
+
+		num->word = "hello";
+		num->count = position;
+		num->next = (struct word_t *)malloc(sizeof(struct word_t));
+		num->next->word = "goodbye";
+		num->next->count = 1;
+
+		write(fd[1], num, sizeof(struct word_t) * 2);
+		close(fd[1]);
+		exit(0);
+
+	}
+
+
 }
